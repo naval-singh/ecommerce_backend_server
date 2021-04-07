3b407c5e5c61a37dac9fa4dd81afe34f0ed7c88f
Naval Verma <navalverma999@gmail.com>
2021-04-07 15:30:30 +0530

    cart api updated

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,4 +1,13 @@
+const { response } = require("express");
 const Cart = require("../models/cart");
+
+const runUpdate = (condition, updation) => {
+    return new Promise((resolve, reject) => {
+        Cart.findOneAndUpdate(condition, updation, { upsert: true })
+            .then((result) => resolve())
+            .catch((err) => reject(err));
+    });
+};
 
 exports.addItemToCart = (req, res) => {
     const { cartItems } = req.body;
@@ -7,38 +16,34 @@ exports.addItemToCart = (req, res) => {
         if (error) {
             return res.status(200).json({ status: false, error });
         } else if (existingCart) {
-            let condition, updation;
-            const item = existingCart.cartItems.find((c) => c.product == cartItems.product);
-            if (item) {
-                condition = { user: req.user._id, "cartItems.product": cartItems.product };
-                updation = {
-                    $set: {
-                        "cartItems.$": {
-                            ...cartItems,
-                            quantity: item.quantity + cartItems.quantity,
+            let promiseArray = [];
+            cartItems.forEach((cartItem) => {
+                let condition, updation;
+                const item = existingCart.cartItems.find((c) => c.product == cartItem.product);
+                if (item) {
+                    condition = { user: req.user._id, "cartItems.product": cartItem.product };
+                    updation = {
+                        $set: {
+                            "cartItems.$": cartItem,
                         },
-                    },
-                };
-            } else {
-                condition = { user: req.user._id };
-                updation = {
-                    $push: {
-                        cartItems: cartItems,
-                    },
-                };
-            }
-            Cart.findOneAndUpdate(condition, updation).exec((error, updatedCart) => {
-                if (error) {
-                    return res.status(200).json({ status: false, error });
+                    };
+                } else {
+                    condition = { user: req.user._id };
+                    updation = {
+                        $push: {
+                            "cartItems": cartItem,
+                        },
+                    };
                 }
-                if (updatedCart) {
-                    return res.status(200).json({ status: true, updatedCart });
-                }
+                promiseArray.push(runUpdate(condition, updation));
             });
+            Promise.all(promiseArray)
+                .then((response) => res.status(200).json({ status: true, response }))
+                .catch((error) => res.status(200).json({ status: false, error }));
         } else {
             const cartObj = {
                 user: req.user._id,
-                cartItems: [cartItems],
+                cartItems: cartItems,
             };
             const _cart = new Cart(cartObj);
             _cart.save((error, newlyAddedCart) => {
@@ -52,3 +57,80 @@ exports.addItemToCart = (req, res) => {
         }
     });
 };
+
+exports.getCartItems = (req, res) => {
+    Cart.findOne({ user: req.user._id })
+    .populate({path: 'cartItems.product', select: '_id name quantity price listPrice productPictures'})
+    .exec((error, cart) => {
+        if (error) {
+            return res.status(200).json({ status: false, error });
+        } else if (cart) {
+            let cartItems = {};
+            cart.cartItems.map((item)=>{
+                cartItems[item.product._id] = {
+                    _id: item.product._id,
+                    name: item.product.name,
+                    qty: item.quantity,
+                    stock: item.product.quantity,
+                    price: item.product.price,
+                    listPrice: item.product.listPrice,
+                    img: item.product.productPictures[0]
+                }
+            })
+            return res.status(200).json({ status: true, cartItems });
+        }
+    });
+};
+
+// exports.addItemToCart = (req, res) => {
+//     const { cartItems } = req.body;
+
+//     Cart.findOne({ user: req.user._id }).exec((error, existingCart) => {
+//         if (error) {
+//             return res.status(200).json({ status: false, error });
+//         } else if (existingCart) {
+//             let condition, updation;
+//             const item = existingCart.cartItems.find((c) => c.product == cartItems.product);
+//             if (item) {
+//                 condition = { user: req.user._id, "cartItems.product": cartItems.product };
+//                 updation = {
+//                     $set: {
+                        // "cartItems.$": {
+                        //     ...cartItems,
+                        //     quantity: item.quantity + cartItems.quantity,
+                        // },
+//                     },
+//                 };
+//             } else {
+//                 condition = { user: req.user._id };
+//                 updation = {
+//                     $push: {
+//                         cartItems: cartItems,
+//                     },
+//                 };
+//             }
+//             Cart.findOneAndUpdate(condition, updation).exec((error, updatedCart) => {
+//                 if (error) {
+//                     return res.status(200).json({ status: false, error });
+//                 }
+//                 if (updatedCart) {
+//                     return res.status(200).json({ status: true, updatedCart });
+//                 }
+//             });
+//         } else {
+//             const cartObj = {
+//                 user: req.user._id,
+//                 cartItems: [cartItems],
+//             };
+//             const _cart = new Cart(cartObj);
+//             _cart.save((error, newlyAddedCart) => {
+//                 if (error) {
+//                     return res.status(200).json({ status: false, error });
+//                 }
+//                 if (newlyAddedCart) {
+//                     return res.status(200).json({ status: true, newlyAddedCart });
+//                 }
+//             });
+//         }
+//     });
+// };
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addItemToCart } = require("../controllers/cart");
+const { addItemToCart, getCartItems } = require("../controllers/cart");
 const { requireSignin, userCheck } = require("../middleware");
 const router = express.Router();
 
 router.post("/addtocart", requireSignin, userCheck, addItemToCart);
+router.post("/getfromcart", requireSignin, userCheck, getCartItems);
 
 module.exports = router;
